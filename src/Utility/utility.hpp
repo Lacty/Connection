fb68afcb6449f8304912a226d686a24646f888b5
Lacty <akira206@gmail.com>
2016-03-23 15:36:09 +0900

    add addArithmeticDataToVector

--- src/Utility/utility.hpp
+++ src/Utility/utility.hpp
@@ -21,3 +21,14 @@ Vec convertArrayToVec(T* array) {
   
   return v;
 }
+
+template<typename T, std::enable_if_t<std::is_arithmetic<T>::value, nullptr_t> = nullptr>
+std::vector<char> addArithmeticDataToVector(T src, std::vector<char> data) {
+  std::string str;
+  str = std::to_string(src);
+  for (size_t i = 0; i < str.size(); i++) {
+    data.push_back((char)str[i]);
+  }
+  data.push_back(NULL); // add NULL for punctuation
+  return std::move(data);
+}
